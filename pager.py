7eb4f8cded9813760dc6169cc8682bbe8e2f9549
git-henrique <henriqueoliveirag@alu.ufc.br>
2025-12-14 18:22:50 -0300

    Arquivo pager para chamar os algoritmos

---
+++ pager.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+
+import argparse
+
+from src.fifo import fifo
+# depois você adiciona:
+# from lru import lru
+# from optimal import optimal
+# etc.
+
+
+def load_trace(filename):
+    trace = []
+    with open(filename) as f:
+        for line in f:
+            trace.append(int(line.strip()))
+    return trace
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Simulador de algoritmos de substituição de páginas"
+    )
+
+    parser.add_argument("--algo", required=True,
+                        choices=["fifo", "lru", "optimal", "clock", "second", "nru", "lfu", "mfu"],
+                        help="Algoritmo de substituição")
+
+    parser.add_argument("--frames", required=True, type=int,
+                        help="Número de molduras de página")
+
+    parser.add_argument("--trace", required=True,
+                        help="Arquivo de trace")
+
+    parser.add_argument("--verbose", action="store_true",
+                        help="Modo verboso")
+
+    args = parser.parse_args()
+
+    trace = load_trace(args.trace)
+
+    if args.algo == "fifo":
+        result = fifo(trace, args.frames)
+
+    # elif args.algo == "lru":
+    #     result = lru(trace, args.frames)
+
+    else:
+        print("Algoritmo ainda não implementado.")
+        return
+
+    print(f"Algoritmo: {args.algo.upper()}")
+    print(f"Frames: {args.frames}")
+    print(f"Referências: {len(trace)}")
+    print(f"Faltas de página: {result['faltas']}")
+    print(f"Taxa de faltas: {result['taxa_faltas']:.2f}%")
+    print(f"Evicções: {result['eviccoes']}")
+
+    if args.verbose:
+        print("Conjunto residente final:")
+        print("Pages:", result["memoria_final"])
+
+
+if __name__ == "__main__":
+    main()
